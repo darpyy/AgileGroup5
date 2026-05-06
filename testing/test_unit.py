bde297bc4fd869aa795be52c6ff3e8c9cb5a66ad
Kelvin Kuang <kelvinkuang2001@gmail.com>
2026-05-06 10:53:33 -0700

    tests import

--- testing/test_unit.py
+++ testing/test_unit.py
@@ -1,8 +1,20 @@
+from flask import Flask, render_template, session, redirect, request, flash, url_for, app
+import json
+import os
+import sqlite3, hashlib #for talking to relational database
+from sqlitedb import startServer
+from datetime import datetime
 import pytest
-from app import app
-from forms import loginForm, RegistrationForm
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
+
+def test_new_posts(client):
+    response = client.get('/posts/new')
+    with open("posts.json", 'r') as f:
+        posts = json.load(f)
+    assert response.status_code == 200
+    assert len(posts) == 1;
+
