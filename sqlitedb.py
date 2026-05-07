552168d25fc597841d22d6fa1845610d053412d7
sam <jblair34@bcit.ca>
2026-05-07 01:10:55 -0700

    added admin page where requests can be viewed

--- sqlitedb.py
+++ sqlitedb.py
@@ -4,17 +4,40 @@ def startServer():
     connection = sqlite3.connect("users.db")
     cursor = connection.cursor()
 
-    cursor.execute(""" 
+    cursor.executescript(""" 
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY,
         email VARCHAR (255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
         username VARCHAR (255) UNIQUE NOT NULL
-    )
+    );
+    CREATE TABLE IF NOT EXISTS activities (
+        actid INTEGER PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description VARCHAR(255) NOT NULL
+    );
+    CREATE TABLE IF NOT EXISTS members (
+        actid INTEGER NOT NULL,
+        id INTEGER NOT NULL,
+        PRIMARY KEY (actid, id),
+        FOREIGN KEY (id) REFERENCES users(id),
+        FOREIGN KEY (actid) REFERENCES activities(actid)
+    );
+    CREATE TABLE IF NOT EXISTS requests (
+        reqid INTEGER PRIMARY KEY,
+        id INTEGER NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        desc VARCHAR(255) NOT NULL,
+        FOREIGN KEY (id) REFERENCES users(id)
+    );
     """)
 
     id1, email1, password1, username1 = 0, "admin@me.ca", hashlib.sha256("123".encode()).hexdigest(), "admin"
     cursor.execute("INSERT OR IGNORE INTO users (id, email, password, username) VALUES (?, ?, ?, ?)", (id1, email1, password1, username1))
+
+    reqid, id, title, desc = 0, 0, "my 1st post", "hi"
+    cursor.execute("INSERT OR IGNORE INTO requests (reqid, id, title, desc) VALUES (?, ?, ?, ?)", (reqid, id, title, desc))
+
 
     connection.commit()
     connection.close()
