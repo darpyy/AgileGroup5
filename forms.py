673888898a4d54435ddac93e5a2e0d2bf37f7120
sam <jblair34@bcit.ca>
2026-05-07 13:25:00 -0700

    updated admin page

--- forms.py
+++ forms.py
@@ -21,9 +21,19 @@ class loginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
-    # a class form for the posts
+
+# a class form for the posts
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(max=120)])
     body  = TextAreaField('Body',  validators=[DataRequired()])
     submit = SubmitField('Post')
+
+
+# a class form for creating activities
+
+class ActivityForm(FlaskForm):
+
+    title = StringField('Title', validators=[DataRequired(), Length(min=2, max=20)])
+    description = StringField('Description', validators=[DataRequired(), Length(min=5, max=100)])
+    submit = SubmitField('Create')
