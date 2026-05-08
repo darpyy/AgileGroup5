99597723cc3c2b39bcdfcbf5b70b4ae1da0350a2
alexa2s <aahmadi45@my.bcit.com>
2026-05-07 19:05:38 -0700

    Did a lot of things

--- sqlitedb.py
+++ sqlitedb.py
@@ -4,33 +4,39 @@ def startServer():
     connection = sqlite3.connect("users.db")
     cursor = connection.cursor()
     cursor.executescript(""" 
->>>>>>> 994f687180ebce53a584dcc0b1c4eff5fec10e9c
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY,
-        email VARCHAR (255) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        username VARCHAR (255) UNIQUE NOT NULL
-    );
-    CREATE TABLE IF NOT EXISTS activities (
-        actid INTEGER PRIMARY KEY,
-        title VARCHAR(255) NOT NULL,
-        description VARCHAR(255) NOT NULL
-    );
-    CREATE TABLE IF NOT EXISTS members (
-        actid INTEGER NOT NULL,
-        id INTEGER NOT NULL,
-        PRIMARY KEY (actid, id),
-        FOREIGN KEY (id) REFERENCES users(id),
-        FOREIGN KEY (actid) REFERENCES activities(actid)
-    );
-    CREATE TABLE IF NOT EXISTS requests (
-        reqid INTEGER PRIMARY KEY,
-        id INTEGER NOT NULL,
-        title VARCHAR(255) NOT NULL,
-        desc VARCHAR(255) NOT NULL,
-        FOREIGN KEY (id) REFERENCES users(id)
-    );
-    """)
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    email VARCHAR (255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    username VARCHAR (255) UNIQUE NOT NULL,
+    city VARCHAR(255)
+);
+CREATE TABLE IF NOT EXISTS activities (
+    actid INTEGER PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS members (
+    actid INTEGER NOT NULL,
+    id INTEGER NOT NULL,
+    PRIMARY KEY (actid, id),
+    FOREIGN KEY (id) REFERENCES users(id),
+    FOREIGN KEY (actid) REFERENCES activities(actid)
+);
+CREATE TABLE IF NOT EXISTS requests (
+    reqid INTEGER PRIMARY KEY,
+    id INTEGER NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    desc VARCHAR(255) NOT NULL,
+    FOREIGN KEY (id) REFERENCES users(id)
+);
+CREATE TABLE IF NOT EXISTS user_tags (
+    id INTEGER NOT NULL,
+    tag VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id, tag),
+    FOREIGN KEY (id) REFERENCES users(id)
+);
+""")
 
     #not for testing, do not remove
     id1, email1, password1, username1 = 0, "admin@me.ca", hashlib.sha256("123".encode()).hexdigest(), "admin"
