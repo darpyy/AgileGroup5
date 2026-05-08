c26c8723e9d3324207dd00099a3c1c6c391d48dc
sam <jblair34@bcit.ca>
2026-05-08 01:43:48 -0700

    existing and requested activities now work, as well as creating an activity

--- forms.py
+++ forms.py
@@ -9,7 +9,7 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Password', validators=[DataRequired()])
+    confirmPassword = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign_Up')
 
 class loginForm(FlaskForm):
@@ -21,7 +21,6 @@ class loginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
-
 # a class form for the posts
 
 class PostForm(FlaskForm):
@@ -30,7 +29,15 @@ class PostForm(FlaskForm):
     submit = SubmitField('Post')
 
 
-# a class form for creating activities
+# a class form for creating requests
+
+class RequestForm(FlaskForm):
+
+    reqtitle = StringField('Title', validators=[DataRequired(), Length(min=2, max=20)])
+    reqdescription = StringField('Description', validators=[DataRequired(), Length(min=5, max=100)])
+    submit = SubmitField('Request')
+
+    # a class form for creating activities
 
 class ActivityForm(FlaskForm):
 
